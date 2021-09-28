77e0b916598a4843ce53c30b791d34be61061853
Jack <2992075490@qq.com>
2021-09-28 21:56:04 +0800

    sql

--- 11_PyMySQL.py
+++ 11_PyMySQL.py
@@ -1,24 +1,28 @@
 import pymysql
 
 def main():
+    #1.连接数据库
     conn = pymysql.connect(host='192.168.244.1',port=3306,
                            user='root',password='123456',
                             db='school',charset='utf8')
     try:
         #上下文语法，可自动关闭游标
-        with conn.cursor() as cursor:  #cursor游标
+        with conn.cursor() as cursor:  #2.获取cursor(游标对象)
+            #3.执行SQL得到结果
             result = cursor.execute(
-                'insert into tb_student values (1234,"卡萨丁",1,"1890-1-1","广东东莞",3)')
+                'delete from tb_student where stuname="卡萨丁"')
             if result==1:
                 print('添加成功！')
-                # 需要手动提交，此时数据库里的内容也会相应改变
+                # 4.手动提交，此时数据库里的内容也会相应改变
                 conn.commit()
             else:
                 print('添加失败！')
     except pymysql.MySQLError as error:
         print(error)
+        #4.操作失败进行回滚
         conn.rollback()
     finally:
+        #关闭连接释放资源
         conn.close()
 
 
