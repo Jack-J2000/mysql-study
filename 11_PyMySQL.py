b257c40e2cb849fc8eb1a9becf534a98b0f468a1
Jack <2992075490@qq.com>
2021-10-01 14:34:27 +0800

    sql

--- 11_PyMySQL.py
+++ 11_PyMySQL.py
@@ -9,8 +9,8 @@ def main():
         #上下文语法，可自动关闭游标
         with conn.cursor() as cursor:  #2.获取cursor(游标对象)
             #3.执行SQL得到结果
-            result = cursor.execute(
-                'delete from tb_student where stuname="卡萨丁"')
+            result = cursor.execute(    #在cursor.execute里写入SQL语句
+                'insert into tb_college (collid,collname,collmaster,collweb) values (4,"皮特沃夫","凯瑟琳","http://www.func.com")')
             if result==1:
                 print('添加成功！')
                 # 4.手动提交，此时数据库里的内容也会相应改变
@@ -24,12 +24,7 @@ def main():
     finally:
         #关闭连接释放资源
         conn.close()
-
-
     print(conn)
-
-
-
 
 if __name__ == '__main__':
     main()
